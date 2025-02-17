270d6317721c3bf135990e600d5d296da774ae8b
Slowlydev <me@slowly.dev>
2025-02-17 19:57:27 +0100

    fix: api changes

--- src/parse/pointers.rs
+++ src/parse/pointers.rs
@@ -10,10 +10,10 @@ pub const ARTIST_PRODUCT_THUMBS: &str =
 pub const ARTIST_ALBUM_YEAR: &str = "/musicTwoRowItemRenderer/subtitle/runs/2/text";
 pub const ARTIST_SINGLE_YEAR: &str = "/musicTwoRowItemRenderer/subtitle/runs/0/text";
 
-pub const ALBUM_NAME: &str = "/header/musicDetailHeaderRenderer/title/runs/0/text";
-pub const ALBUM_YEAR: &str = "/header/musicDetailHeaderRenderer/subtitle/runs/4/text";
-pub const ALBUM_THUMBS: &str = "/header/musicDetailHeaderRenderer/thumbnail/croppedSquareThumbnailRenderer/thumbnail/thumbnails";
-pub const ALBUM_TRACKS: &str = "/contents/singleColumnBrowseResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/0/musicShelfRenderer/contents";
+pub const ALBUM_NAME: &str = "/contents/twoColumnBrowseResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/0/musicResponsiveHeaderRenderer/title/runs/0/text";
+pub const ALBUM_YEAR: &str = "/contents/twoColumnBrowseResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/0/musicResponsiveHeaderRenderer/subtitle/runs/0/text";
+pub const ALBUM_THUMBS: &str = "/contents/twoColumnBrowseResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/0/musicResponsiveHeaderRenderer/thumbnail/musicThumbnailRenderer/thumbnail/thumbnails";
+pub const ALBUM_TRACKS: &str = "/contents/twoColumnBrowseResultsRenderer/secondaryContents/sectionListRenderer/contents/0/musicShelfRenderer/contents/0/musicResponsiveListItemRenderer/flexColumns";
 pub const ALBUM_TRACK_NAME: &str = "/musicResponsiveListItemRenderer/flexColumns/0/musicResponsiveListItemFlexColumnRenderer/text/runs/0/text";
 pub const ALBUM_TRACK_ID: &str = "/musicResponsiveListItemRenderer/flexColumns/0/musicResponsiveListItemFlexColumnRenderer/text/runs/0/navigationEndpoint/watchEndpoint/videoId";
 
@@ -21,7 +21,7 @@ pub const THUMBNAIL_URL: &str = "/url";
 pub const THUMBNAIL_HEIGHT: &str = "/height";
 pub const THUMBNAIL_WIDTH: &str = "/width";
 
-pub const SEARCHED_ARTISTS: &str = "/contents/tabbedSearchResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/1/musicShelfRenderer/contents";
+pub const SEARCHED_ARTISTS: &str = "/contents/tabbedSearchResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/0/musicShelfRenderer/contents";
 pub const SEARCHED_ARTIST_NAME: &str = "/musicResponsiveListItemRenderer/flexColumns/0/musicResponsiveListItemFlexColumnRenderer/text/runs/0/text";
 pub const SEARCHED_ARTIST_SUBS: &str = "/musicResponsiveListItemRenderer/flexColumns/1/musicResponsiveListItemFlexColumnRenderer/text/runs/2/text";
 pub const SEARCHED_ARTIST_ID: &str =
